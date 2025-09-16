d7cacbbb7a4596e7f5dd021f0b0b95c3a5cc58bc
taiyuuki <taiyuuki@qq.com>
2025-09-16 17:35:44 +0800

    refactor: 优化代码格式，合并数组元素，移除冗余行

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,13 @@ import tyk_eslint from '@taiyuuki/eslint-config'
 
 export default tyk_eslint({
     ts: true,
-    vue: true,
     rules: {
-        'no-undef': 'off',
-    }
-}, {
-    files: ['scripts/**/*.ts'],
-    rules: { 
         'no-console': 'off',
-     },
+        '@stylistic/array-element-newline': 'off', 
+    },
+    markdown: false,
+    ignores: ['examples/node-example.js', '**/*.md'],
+}, {
+    files: ['**/*.js'],
+    rules: { 'no-undef': 'off' },
 })
