529c41e3211f475d6e2985e43d9887c9bfe85da7
taiyuuki <taiyuuki@qq.com>
2026-04-03 13:51:22 +0800

    chore: update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
 import tyk_eslint from '@taiyuuki/eslint-config'
 
 export default tyk_eslint({
-    ts: true,
-    vue: true,
-    jsx: true,
-    reactVersion: "19.1.1",
-    rules: {
-        'no-console': 'off',
-        'no-undef': 'off',
+    ts:           true,
+    vue:          true,
+    jsx:          true,
+    reactVersion: '19.1.1',
+    rules:        {
+        'no-console':                       'off',
+        'no-undef':                         'off',
         '@stylistic/array-element-newline': 'off', 
     },
     markdown: false,
-    ignores: ['examples/node-example.js', '**/*.md', '**/*.css', '**/cache/**', '**/_log/**'],
+    ignores:  ['examples/node-example.js', '**/*.md', '**/*.css', '**/cache/**', '**/_log/**'],
 }, {
     files: ['**/*.js'],
     rules: { 'no-undef': 'off' },
