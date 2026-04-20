78cdf6b227e01563eef90b7e6312e03010b88bed
taiyuuki <taiyuuki@qq.com>
2026-04-20 22:31:53 +0800

    docs: remove react

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -15,7 +15,6 @@ export default {
                     { text: '@nesjs/core', link: '/guide/core' },
                     { text: '@nesjs/native', link: '/guide/native' },
                     { text: '@nesjs/vue3', link: '/guide/vue3' },
-                    { text: '@nesjs/react', link: '/zh/guide/react' },
                 ],
             },
         ],
